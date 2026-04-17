705d0179fcd66f424cca56331e3f488ab44bc0e8
Lawrence Lau <m@psi.tv>
2026-04-18 00:51:03 +0800

    build: guard MEMORY_FEX_STATS_SHM_INFORMATION typedef to avoid redefinition during CI

--- include/winternl.h
+++ include/winternl.h
@@ -2533,11 +2533,15 @@ typedef struct _MEMORY_IMAGE_INFORMATION
     };
 } MEMORY_IMAGE_INFORMATION, *PMEMORY_IMAGE_INFORMATION;
 
+/* Prevent duplicate definition when patches or other headers add this type */
+#ifndef _MEMORY_FEX_STATS_SHM_INFORMATION
+#define _MEMORY_FEX_STATS_SHM_INFORMATION
 typedef struct _MEMORY_FEX_STATS_SHM_INFORMATION
 {
     void *shm_base;
     SIZE_T map_size;
 } MEMORY_FEX_STATS_SHM_INFORMATION, *PMEMORY_FEX_STATS_SHM_INFORMATION;
+#endif
 
 typedef enum _MUTANT_INFORMATION_CLASS
 {
